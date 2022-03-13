f4ce7bd8e95e5a84a9b8b517a5726b1c7ffd7e83
bboyfengting <1832224956@qq.com>
2022-03-13 18:19:18 +0800

    second

--- realtime/src/main/scala/cn/bigdatabc/realtime/app/Dauapp.scala
+++ realtime/src/main/scala/cn/bigdatabc/realtime/app/Dauapp.scala
@@ -3,7 +3,7 @@ package cn.bigdatabc.realtime.app
 import cn.bigdatabc.realtime.util.MyKafkaUtil
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 /**
@@ -11,17 +11,19 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
  * @date 2022/3/12
  */
 object Dauapp {
-    private val sparkConf: SparkConf = new SparkConf().setMaster("local[2]").setAppName("DauApp")
-    private val streamingContext = new StreamingContext(sparkConf, Seconds(5))
+    def main(args: Array[String]): Unit = {
+        val sparkConf: SparkConf = new SparkConf().setMaster("local[2]").setAppName("DauApp")
+        val streamingContext = new StreamingContext(sparkConf, Seconds(5))
 
-    /**
-     * 消费Kafka数据基本实现
-     */
-    var topic:String = "gmall_start_0523"
-    var groupId:String = "gmall_dau_0523"
-    val kafkaDStream: InputDStream[ConsumerRecord[String, String]] = MyKafkaUtil.getKafkaStream(topic, streamingContext, groupId)
-    kafkaDStream.print()
+        /**
+          * 消费Kafka数据基本实现
+          */
+        var topic:String = "gmall_start_0523"
+        var groupId:String = "gmall_dau_0523"
+        val kafkaDStream: InputDStream[ConsumerRecord[String, String]] = MyKafkaUtil.getKafkaStream(topic, streamingContext, groupId)
+        kafkaDStream.map(_.value()).print(10)
 
-    streamingContext.start()
-    streamingContext.awaitTermination()
+        streamingContext.start()
+        streamingContext.awaitTermination()
+    }
 }
